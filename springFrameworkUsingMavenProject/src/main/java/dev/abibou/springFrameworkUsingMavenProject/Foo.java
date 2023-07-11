d0def799911f62350a33278000780639a1b4f38d
abibou1 <abibou2017@gmail.com>
2023-07-11 07:14:23 -0400

    added spring framework app using maven project

---
+++ springFrameworkUsingMavenProject/src/main/java/dev/abibou/springFrameworkUsingMavenProject/Foo.java
@@ -0,0 +1,28 @@
+package dev.abibou.springFrameworkUsingMavenProject;
+
+public class Foo {
+	private String name;
+	
+	public Foo() {}
+	
+	public Foo(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Foo [name=" + name + "]";
+	}
+	
+	
+
+}
+
