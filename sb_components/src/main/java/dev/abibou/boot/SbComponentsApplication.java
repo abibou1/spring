44dd98bd3c47c5f580aa51096a4e32f7e5046328
abibou1 <abibou2017@gmail.com>
2023-07-26 02:34:58 -0400

    added spring boot components demos

---
+++ sb_components/src/main/java/dev/abibou/boot/SbComponentsApplication.java
@@ -0,0 +1,13 @@
+package dev.abibou.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbComponentsApplication.class, args);
+	}
+
+}
