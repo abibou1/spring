c80e35d144178ee47932a9b47dfaa19326996080
abibou1 <abibou2017@gmail.com>
2023-08-10 02:01:00 -0400

    generate rest project

---
+++ sboot_rest_responseentity/src/test/java/dev/abibou/security/SbootRestResponseentityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abibou.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootRestResponseentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
