46730821c6b032cf90eab1e537c7cffca603afa2
abibou1 <abibou2017@gmail.com>
2023-08-10 02:48:02 -0400

    generate boot project for query params and headers

---
+++ sboot_rest_query_params_headers/src/main/java/dev/abibou/rest/SbootRestQueryParamsHeadersApplication.java
@@ -0,0 +1,13 @@
+package dev.abibou.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootRestQueryParamsHeadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootRestQueryParamsHeadersApplication.class, args);
+	}
+
+}
---
+++ sboot_rest_query_params_headers/src/test/java/dev/abibou/rest/SbootRestQueryParamsHeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abibou.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootRestQueryParamsHeadersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
